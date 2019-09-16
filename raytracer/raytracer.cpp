e063907ab2bf59ccd7ed9470afe417de499ce725
Denis Konoplev <dekonoplyov@gmail.com>
2019-09-17 01:10:17 +0300

    More spheres

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -1,6 +1,7 @@
 #include "geometry.h"
 
 #include <fstream>
+#include <limits>
 #include <string>
 #include <vector>
 
@@ -8,11 +9,27 @@ const int WIDTH = 1280;
 const int HEIGHT = 720;
 const int FOV = M_PI / 2.;
 
+struct Material {
+public:
+    Material(const Vec3f& color)
+        : diffuseColor(color)
+    {
+    }
+    Material()
+        : diffuseColor()
+    {
+    }
+
+public:
+    Vec3f diffuseColor;
+};
+
 struct Sphere {
 public:
-    Sphere(const Vec3f c, const float r)
+    Sphere(const Vec3f c, const float r, const Material m)
         : center{c}
         , radius{r}
+        , material{m}
     {
     }
 
@@ -37,6 +54,7 @@ public:
 public:
     Vec3f center;
     float radius;
+    Material material;
 };
 
 void writePPM(const std::string& filepath, const std::vector<Vec3f>& framebuffer)
@@ -54,18 +72,37 @@ void writePPM(const std::string& filepath, const std::vector<Vec3f>& framebuffer
     }
 }
 
-Vec3f castRay(const Vec3f& orig, const Vec3f& dir, const Sphere& sphere)
+bool sceneIntersect(const Vec3f& orig, const Vec3f& dir,
+    const std::vector<Sphere>& spheres,
+    Vec3f& hit, Vec3f& N, Material& material)
 {
-    float sphereDist = 0.f;
-    if (!sphere.rayIntersect(orig, dir, sphereDist)) {
-        return Vec3f(0.2, 0.7, 0.8); // background color
+    float spheresDist = std::numeric_limits<float>::max();
+    for (const auto& sphere : spheres) {
+        float dist = 0.f;
+        if (sphere.rayIntersect(orig, dir, dist) && dist < spheresDist) {
+            spheresDist = dist;
+            hit = orig + dir * dist;
+            N = (hit - sphere.center).normalize();
+            material = sphere.material;
+        }
     }
-    return Vec3f(0.4, 0.4, 0.3);
+
+    return spheresDist < 1000.f;
 }
 
-void render(const std::string& filepath, const Sphere& sphere)
+Vec3f castRay(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& spheres)
 {
+    Vec3f point, N;
+    Material material;
 
+    if (!sceneIntersect(orig, dir, spheres, point, N, material)) {
+        return Vec3f(0.2, 0.7, 0.8); // background color
+    }
+    return material.diffuseColor;
+}
+
+void render(const std::string& filepath, const std::vector<Sphere>& spheres)
+{
     std::vector<Vec3f> framebuffer(WIDTH * HEIGHT);
 
     for (int j = 0; j < HEIGHT; ++j) {
@@ -74,7 +111,7 @@ void render(const std::string& filepath, const Sphere& sphere)
             float y = -(j + 0.5) + HEIGHT / 2.;
             float z = -HEIGHT / (2. * tan(FOV / 2.));
             Vec3f dir = Vec3f(x, y, z).normalize();
-            framebuffer[i + j * WIDTH] = castRay(Vec3f(0, 0, 0), dir, sphere);
+            framebuffer[i + j * WIDTH] = castRay(Vec3f(0, 0, 0), dir, spheres);
         }
     }
 
@@ -83,7 +120,15 @@ void render(const std::string& filepath, const Sphere& sphere)
 
 int main()
 {
-    Sphere sphere(Vec3f(-3, 0, -16), 2);
-    render("./raytracer/out.ppm", sphere);
+    Material ivory(Vec3f(0.4, 0.4, 0.3));
+    Material redRubber(Vec3f(0.3, 0.1, 0.1));
+
+    std::vector<Sphere> spheres;
+    spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
+    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, redRubber));
+    spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, redRubber));
+    spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, ivory));
+
+    render("./raytracer/out.ppm", spheres);
     return 0;
 }
